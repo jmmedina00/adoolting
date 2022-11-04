78f6b64371daab9eb2aa69d511809e147fa8d774
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-11-04 20:35:32 +0100

    first commit - generated from Spring Initializr

---
+++ src/main/java/io/github/jmmedina00/adoolting/AdooltingApplication.java
@@ -0,0 +1,13 @@
+package io.github.jmmedina00.adoolting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdooltingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdooltingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jmmedina00/adoolting/AdooltingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jmmedina00.adoolting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdooltingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
