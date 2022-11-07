0f14cb95fe7f60f385a19d2169ab0964e9607672
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-11-07 17:35:28 +0100

    chore(testing): add sample authentication config

--- src/main/java/io/github/jmmedina00/adoolting/config/SecurityConfiguration.java
+++ src/main/java/io/github/jmmedina00/adoolting/config/SecurityConfiguration.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,9 +19,40 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http)
     throws Exception {
-    http.authorizeHttpRequests(
-      requests -> requests.antMatchers("/**").permitAll()
-    );
+    http
+      .authorizeHttpRequests(
+        requests ->
+          requests
+            .antMatchers("/test/**")
+            .authenticated()
+            .antMatchers("/**")
+            .permitAll()
+      )
+      .formLogin()
+      .loginPage("/");
     return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails clara = User
+      .withDefaultPasswordEncoder()
+      .username("clara")
+      .password("test")
+      .roles("USER")
+      .build();
+    UserDetails mario = User
+      .withDefaultPasswordEncoder()
+      .username("mario")
+      .password("1234")
+      .roles("USER")
+      .build();
+
+    return new InMemoryUserDetailsManager(clara, mario);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
---
+++ src/main/java/io/github/jmmedina00/adoolting/controller/MainController.java
@@ -0,0 +1,15 @@
+package io.github.jmmedina00.adoolting.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String hello() {
+    return "hello";
+  }
+}
