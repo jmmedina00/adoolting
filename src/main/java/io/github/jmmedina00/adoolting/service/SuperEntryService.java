3444db83443814f8045c43a8995cf016119d78f2
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-11-09 20:09:21 +0100

    feat(testing): write new name into a random file as bg job

--- src/main/java/io/github/jmmedina00/adoolting/service/SuperEntryService.java
+++ src/main/java/io/github/jmmedina00/adoolting/service/SuperEntryService.java
@@ -2,7 +2,11 @@ package io.github.jmmedina00.adoolting.service;
 
 import io.github.jmmedina00.adoolting.entity.SuperEntry;
 import io.github.jmmedina00.adoolting.repository.SuperEntryRepository;
+import java.io.PrintStream;
 import java.util.List;
+import java.util.Random;
+import org.jobrunr.jobs.annotations.Job;
+import org.jobrunr.scheduling.JobScheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +15,11 @@ public class SuperEntryService {
   @Autowired
   private SuperEntryRepository superEntryRepository;
 
+  @Autowired
+  private JobScheduler jobScheduler;
+
+  private Random random = new Random();
+
   public List<SuperEntry> getEntries() {
     return superEntryRepository.findByDeletedAtIsNull();
   }
@@ -18,11 +27,24 @@ public class SuperEntryService {
   public void createEntry(String name) {
     SuperEntry entry = new SuperEntry(name);
     superEntryRepository.save(entry);
+    jobScheduler.enqueue(() -> writeNameToFile(name));
   }
 
   public void deleteEntry(Long id) {
     SuperEntry entry = superEntryRepository.getReferenceById(id);
     entry.delete();
     superEntryRepository.save(entry);
+  }
+
+  @Job(name = "Write to file")
+  public void writeNameToFile(String name) throws Exception {
+    PrintStream ps = new PrintStream(
+      "./data/cdn/test/" + random.nextInt() + ".txt"
+    );
+    ps.println(name);
+    ps.println(name.toUpperCase());
+    ps.println(name.toLowerCase());
+    ps.println(name.intern());
+    ps.close();
   }
 }
