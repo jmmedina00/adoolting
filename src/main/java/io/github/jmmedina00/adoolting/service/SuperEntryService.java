8472fb60672a71386f77dfc99433fca91679159e
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-11-11 14:09:38 +0100

    chore(testing): ensure images are processed nicer add interpolation and account for transparent and non-transparent images

--- src/main/java/io/github/jmmedina00/adoolting/service/SuperEntryService.java
+++ src/main/java/io/github/jmmedina00/adoolting/service/SuperEntryService.java
@@ -2,15 +2,22 @@ package io.github.jmmedina00.adoolting.service;
 
 import io.github.jmmedina00.adoolting.entity.SuperEntry;
 import io.github.jmmedina00.adoolting.repository.SuperEntryRepository;
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Random;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageOutputStream;
 import org.apache.commons.io.FilenameUtils;
 import org.jobrunr.jobs.annotations.Job;
 import org.jobrunr.scheduling.JobScheduler;
@@ -50,7 +57,8 @@ public class SuperEntryService {
 
   public void saveImage(MultipartFile file) throws Exception {
     String extension = FilenameUtils.getExtension(file.getOriginalFilename());
-    String path = workDirectory + "/data/cdn/test/" + random.nextInt() + "." + extension;
+    String path =
+      workDirectory + "/data/cdn/test/" + random.nextInt() + "." + extension;
 
     File fileToWrite = new File(path);
     file.transferTo(fileToWrite);
@@ -70,31 +78,49 @@ public class SuperEntryService {
   }
 
   @Job(name = "Scale image")
-  public void scaleImage(String path) throws Exception {
-    String filename = FilenameUtils.getName(path);
+  public void scaleImage(String pathString) throws Exception {
+    String filename = FilenameUtils.getName(pathString);
+    Path path = new File(pathString).toPath();
+    String type = Files.probeContentType(path);
+    ImageWriter writer = ImageIO.getImageWritersByMIMEType(type).next();
 
-    InputStream stream = new FileInputStream(path);
+    InputStream stream = new FileInputStream(pathString);
     BufferedImage source = ImageIO.read(stream);
+    int imageType = source.getColorModel().hasAlpha()
+      ? BufferedImage.TYPE_4BYTE_ABGR
+      : source.getType(); // Account for transparent image cases, fallback for JPGs - writer doesn't like it
 
     // https://stackoverflow.com/questions/4756268/how-to-resize-the-buffered-image-n-graphics-2d-in-java
-    BufferedImage target128 = new BufferedImage(128, 128, source.getType());
+    BufferedImage target128 = new BufferedImage(128, 128, imageType);
     Graphics2D graphics128 = target128.createGraphics();
+    graphics128.setRenderingHint(
+      RenderingHints.KEY_INTERPOLATION,
+      RenderingHints.VALUE_INTERPOLATION_BICUBIC
+    );
+    graphics128.setBackground(new Color(0, 0, 0, 0));
     graphics128.drawImage(source, 0, 0, 128, 128, null);
     graphics128.dispose();
-    ImageIO.write(
-      target128,
-      "jpeg",
+    ImageOutputStream output128 = new FileImageOutputStream(
       new File("./data/cdn/test/big/" + filename)
     );
+    writer.setOutput(output128);
+    writer.write(target128);
+    output128.close();
 
-    BufferedImage target64 = new BufferedImage(64, 64, source.getType());
+    BufferedImage target64 = new BufferedImage(64, 64, imageType);
     Graphics2D graphics64 = target64.createGraphics();
+    graphics64.setRenderingHint(
+      RenderingHints.KEY_INTERPOLATION,
+      RenderingHints.VALUE_INTERPOLATION_BICUBIC
+    );
+    graphics64.setBackground(new Color(0, 0, 0, 0));
     graphics64.drawImage(source, 0, 0, 64, 64, null);
     graphics64.dispose();
-    ImageIO.write(
-      target64,
-      "jpeg",
+    ImageOutputStream output64 = new FileImageOutputStream(
       new File("./data/cdn/test/smol/" + filename)
     );
+    writer.setOutput(output64);
+    writer.write(target64);
+    output64.close();
   }
 }
