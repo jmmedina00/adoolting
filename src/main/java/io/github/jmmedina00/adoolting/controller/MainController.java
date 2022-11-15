5094b15bf030f500e72b5b5199a0d506113110ab
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-11-15 16:41:09 +0100

    chore: add basic User DTO and validation workflow

--- src/main/java/io/github/jmmedina00/adoolting/controller/MainController.java
+++ src/main/java/io/github/jmmedina00/adoolting/controller/MainController.java
@@ -1,11 +1,17 @@
 package io.github.jmmedina00.adoolting.controller;
 
+import io.github.jmmedina00.adoolting.dto.User;
+import javax.validation.Valid;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
@@ -26,11 +32,37 @@ public class MainController {
   }
 
   @RequestMapping(method = RequestMethod.GET)
-  public String hello() {
+  public String hello(Model model) {
     if (isAuthenticated()) {
       return "redirect:test";
     }
+    
+    // Might have been redirected from invalid register
+    if (!model.containsAttribute("user")) {
+      User userDto = new User();
+      model.addAttribute("user", userDto);
+    }
 
     return "hello";
+  }
+
+  @RequestMapping(method = RequestMethod.POST, value = "/register")
+  public String registerNewPerson(
+    @ModelAttribute("user") @Valid User userDto,
+    BindingResult result,
+    RedirectAttributes attributes
+  ) {
+    if (result.hasErrors()) {
+      // Preserve errors and originally input values when redirecting
+      attributes.addFlashAttribute(
+        "org.springframework.validation.BindingResult.user",
+        result
+      );
+      attributes.addFlashAttribute("user", userDto);
+
+      return "redirect:";
+    }
+
+    return "valid";
   }
 }
---
+++ src/main/java/io/github/jmmedina00/adoolting/dto/User.java
@@ -0,0 +1,76 @@
+package io.github.jmmedina00.adoolting.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class User implements Serializable {
+  @NotNull
+  @NotEmpty
+  private String firstName;
+
+  @NotNull
+  @NotEmpty
+  private String lastName;
+
+  @NotNull
+  @NotEmpty
+  private String email;
+
+  private String confirmEmail;
+
+  @NotNull
+  @NotEmpty
+  private String password;
+
+  private String confirmPassword;
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getConfirmEmail() {
+    return confirmEmail;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getConfirmPassword() {
+    return confirmPassword;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setConfirmEmail(String confirmEmail) {
+    this.confirmEmail = confirmEmail;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void setConfirmPassword(String confirmPassword) {
+    this.confirmPassword = confirmPassword;
+  }
+}
