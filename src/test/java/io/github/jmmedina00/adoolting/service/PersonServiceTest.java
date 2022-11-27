98e196393977d71e5246fb0af35bf20f92b34eee
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-11-27 18:22:02 +0100

    build: add functional test configuration; no DBs running needed

---
+++ src/test/java/io/github/jmmedina00/adoolting/service/PersonServiceTest.java
@@ -0,0 +1,39 @@
+package io.github.jmmedina00.adoolting.service;
+
+import io.github.jmmedina00.adoolting.entity.Person;
+import io.github.jmmedina00.adoolting.repository.PersonRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class PersonServiceTest {
+  @MockBean
+  private PersonRepository personRepository;
+
+  @MockBean
+  private PasswordEncoder passwordEncoder;
+
+  @MockBean
+  private ConfirmationService confirmationService;
+
+  @Autowired
+  private PersonService personService;
+
+  @Test
+  public void getPersonByIdReturnsPerson() {
+    Person expected = new Person();
+
+    Mockito
+      .when(personRepository.findById((long) 1))
+      .thenReturn(Optional.of(expected));
+    Assertions.assertEquals(expected, personService.getPerson((long) 1));
+  }
+}
