c4fb3d9ba07fc4a61ca1fcfbcc6c7d2685d675b9
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-11-30 19:42:14 +0100

    test(person): add more unit tests for service

--- src/test/java/io/github/jmmedina00/adoolting/service/PersonServiceTest.java
+++ src/test/java/io/github/jmmedina00/adoolting/service/PersonServiceTest.java
@@ -1,16 +1,29 @@
 package io.github.jmmedina00.adoolting.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import io.github.jmmedina00.adoolting.dto.User;
+import io.github.jmmedina00.adoolting.entity.enums.Gender;
 import io.github.jmmedina00.adoolting.entity.person.Person;
+import io.github.jmmedina00.adoolting.entity.util.PersonDetails;
+import io.github.jmmedina00.adoolting.exception.EmailIsUsedException;
 import io.github.jmmedina00.adoolting.repository.PersonRepository;
 import io.github.jmmedina00.adoolting.service.person.PersonService;
 import io.github.jmmedina00.adoolting.service.util.ConfirmationService;
+import java.util.Date;
 import java.util.Optional;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
 
@@ -36,6 +49,124 @@ public class PersonServiceTest {
     Mockito
       .when(personRepository.findById((long) 1))
       .thenReturn(Optional.of(expected));
-    Assertions.assertEquals(expected, personService.getPerson((long) 1));
+    assertEquals(expected, personService.getPerson((long) 1));
+  }
+
+  @Test
+  public void getPersonByBadIdReturnsNull() {
+    Mockito
+      .when(personRepository.findById((long) 1))
+      .thenReturn(Optional.ofNullable(null));
+    assertNull(personService.getPerson((long) 1));
+  }
+
+  @Test
+  public void changePersonPasswordEncodesPasswordBeforeSaving() {
+    Person person = new Person();
+    String newPassword = "mypassword";
+
+    Mockito
+      .when(personRepository.findById((long) 1))
+      .thenReturn(Optional.of(person));
+    Mockito
+      .when(personRepository.save(any()))
+      .thenAnswer(
+        invocation -> {
+          return invocation.getArgument(0);
+        }
+      );
+    Mockito.when(passwordEncoder.encode(anyString())).thenReturn("ENCODED");
+
+    Person saved = personService.changePersonPassword((long) 1, newPassword);
+
+    assertEquals(saved.getPassword(), "ENCODED");
+
+    verify(passwordEncoder, times(1)).encode(newPassword);
+    verify(personRepository, times(1)).findById((long) 1);
+    verify(personRepository, times(1)).save(person);
+  }
+
+  @Test
+  public void createPersonCreatesPersonFromUserData()
+    throws EmailIsUsedException {
+    String unusedEmail = "juanmi@juanmi.com";
+    User user = new User(); // Service expects valid payload from controller
+    user.setFirstName("Juanmi");
+    user.setLastName("Medina");
+    user.setEmail(unusedEmail);
+    user.setConfirmEmail(unusedEmail);
+    user.setPassword("123456");
+    user.setConfirmPassword("123456");
+    user.setGender(Gender.HE);
+    user.setBirthday(new Date(950659200000L)); // 2000/02/16
+
+    Mockito.when(passwordEncoder.encode(anyString())).thenReturn("ENCODED");
+    Mockito.when(personRepository.findByEmail(unusedEmail)).thenReturn(null);
+    Mockito
+      .when(personRepository.save(any()))
+      .thenAnswer(
+        invocation -> {
+          return invocation.getArgument(0);
+        }
+      );
+
+    Person person = personService.createPersonFromUser(user);
+    assertEquals(user.getFirstName(), person.getFirstName());
+    assertEquals(user.getLastName(), person.getLastName());
+    assertEquals(user.getEmail(), person.getEmail());
+    assertEquals(user.getBirthday(), person.getBirthDate());
+    assertEquals("ENCODED", person.getPassword());
+    assertEquals(user.getGender(), person.getGender());
+
+    verify(passwordEncoder, times(1)).encode(user.getPassword());
+    verify(personRepository, times(1)).save(any());
+    verify(confirmationService, times(1)).createTokenforPerson(person);
+  }
+
+  @Test
+  public void createPersonWithUsedEmailResultsinException() {
+    String usedEmail = "juanmi@juanmi.com";
+    User user = new User(); // Service expects valid payload from controller
+    user.setEmail(usedEmail);
+
+    Mockito
+      .when(personRepository.findByEmail(usedEmail))
+      .thenReturn(new Person());
+
+    assertThrows(
+      EmailIsUsedException.class,
+      () -> {
+        personService.createPersonFromUser(user);
+      }
+    );
+  }
+
+  @Test
+  public void loadUserByUsernameReturnsDetailsFromPersonEmail()
+    throws UsernameNotFoundException {
+    Person person = new Person();
+
+    Mockito
+      .when(personRepository.findByEmail("juanmi@juanmi.com"))
+      .thenReturn(person);
+
+    PersonDetails details = (PersonDetails) personService.loadUserByUsername(
+      "juanmi@juanmi.com"
+    );
+    assertEquals(details.getPerson(), person);
+  }
+
+  @Test
+  public void loadUserByUsernameThrowsCorrectExceptionWhenPersonNotFound() {
+    Mockito
+      .when(personRepository.findByEmail("juanmi@juanmi.com"))
+      .thenReturn(null);
+
+    assertThrows(
+      UsernameNotFoundException.class,
+      () -> {
+        personService.loadUserByUsername("juanmi@juanmi.com");
+      }
+    );
   }
 }
