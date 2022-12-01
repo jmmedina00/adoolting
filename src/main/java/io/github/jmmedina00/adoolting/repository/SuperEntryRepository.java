e6c176109433b7ca5f67cd339153858fd8960d29
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-12-01 11:37:53 +0100

    chore: remove sample code from initial testing

--- src/main/java/io/github/jmmedina00/adoolting/repository/SuperEntryRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.jmmedina00.adoolting.repository;
-
-import io.github.jmmedina00.adoolting.entity.SuperEntry;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SuperEntryRepository extends JpaRepository<SuperEntry, Long> {
-  List<SuperEntry> findByDeletedAtIsNull();
-}
