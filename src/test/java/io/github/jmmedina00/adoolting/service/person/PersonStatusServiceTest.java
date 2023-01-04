3818122656475e833dc9e0805681fd5eb409101a
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-01-04 12:35:51 +0100

    test(status): add unit tests for service

---
+++ src/test/java/io/github/jmmedina00/adoolting/service/person/PersonStatusServiceTest.java
@@ -0,0 +1,52 @@
+package io.github.jmmedina00.adoolting.service.person;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import io.github.jmmedina00.adoolting.entity.person.Person;
+import io.github.jmmedina00.adoolting.entity.person.PersonStatus;
+import io.github.jmmedina00.adoolting.repository.person.PersonStatusRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class PersonStatusServiceTest {
+  @MockBean
+  private PersonStatusRepository statusRepository;
+
+  @Autowired
+  private PersonStatusService statusService;
+
+  @Test
+  public void updatePersonStatusCreatesNewStatusWithPersonAndContent() {
+    String content = "Test";
+    Person person = new Person();
+
+    Mockito
+      .when(statusRepository.save(any()))
+      .thenAnswer(invocation -> invocation.getArgument(0));
+    PersonStatus status = statusService.updatePersonStatus(person, content);
+
+    assertEquals(person, status.getPerson());
+    assertEquals(content, status.getContent());
+  }
+
+  @Test
+  public void updatePersonStatusCreatesNoStatusIfContentIsEmpty() {
+    String content = "";
+    Person person = new Person();
+
+    PersonStatus status = statusService.updatePersonStatus(person, content);
+
+    assertNull(status);
+    verify(statusRepository, never()).save(any());
+  }
+}
