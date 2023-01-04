f6f58f813d223e4042abf5ad3e9b9142d39527d3
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-01-04 19:04:56 +0100

    test(interaction): test appropriate interactor list special scenarios

--- src/test/java/io/github/jmmedina00/adoolting/service/InteractionServiceTest.java
+++ src/test/java/io/github/jmmedina00/adoolting/service/InteractionServiceTest.java
@@ -4,11 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import io.github.jmmedina00.adoolting.entity.Interaction;
+import io.github.jmmedina00.adoolting.entity.Interactor;
+import io.github.jmmedina00.adoolting.entity.group.Event;
+import io.github.jmmedina00.adoolting.entity.interaction.Comment;
 import io.github.jmmedina00.adoolting.entity.interaction.Post;
+import io.github.jmmedina00.adoolting.entity.page.Page;
 import io.github.jmmedina00.adoolting.entity.person.Person;
 import io.github.jmmedina00.adoolting.exception.NotAuthorizedException;
 import io.github.jmmedina00.adoolting.repository.InteractionRepository;
@@ -134,6 +139,115 @@ public class InteractionServiceTest {
       NotAuthorizedException.class,
       () -> {
         interactionService.deleteInteraction(5L, 2L);
+      }
+    );
+  }
+
+  @Test
+  public void getAppropriateInteractorListForPersonReturnsPersonPlusPageWhenItIsACommentPostedByPageAndPersonManagesIt() {
+    Person person = new Person();
+    Page page = new Page();
+    page.setId(20L);
+    Comment comment = new Comment();
+    Event event = new Event();
+    event.setInteractor(page);
+    comment.setReceiverInteraction(event);
+
+    Mockito.when(interactorService.getInteractor(4L)).thenReturn(person);
+    Mockito
+      .when(interactionRepository.findActiveInteraction(120L))
+      .thenReturn(Optional.of(comment));
+    Mockito.when(pageService.isPageManagedByPerson(20L, 4L)).thenReturn(true);
+
+    List<Interactor> possibleInteractors = interactionService.getAppropriateInteractorListForPerson(
+      4L,
+      120L
+    );
+    assertEquals(List.of(person, page), possibleInteractors);
+  }
+
+  @Test
+  public void getAppropriateInteractorListForPersonReturnsPersonWhenItIsACommentPostedByPageAndPersonDoesNotManageIt() {
+    Person person = new Person();
+    Page page = new Page();
+    page.setId(20L);
+    Comment comment = new Comment();
+    Event event = new Event();
+    event.setInteractor(page);
+    comment.setReceiverInteraction(event);
+
+    Mockito.when(interactorService.getInteractor(4L)).thenReturn(person);
+    Mockito
+      .when(interactionRepository.findActiveInteraction(120L))
+      .thenReturn(Optional.of(comment));
+    Mockito.when(pageService.isPageManagedByPerson(20L, 4L)).thenReturn(false);
+
+    List<Interactor> possibleInteractors = interactionService.getAppropriateInteractorListForPerson(
+      4L,
+      120L
+    );
+    assertEquals(List.of(person), possibleInteractors);
+  }
+
+  @Test
+  public void getAppropriateInteractorListForPersonReturnsPersonPlusPageWhenInteractionReceivedByPageManagedByPerson() {
+    Person person = new Person();
+    Page foo = new Page();
+    foo.setId(20L);
+    Page bar = new Page();
+    bar.setId(21L);
+
+    Post post = new Post();
+    post.setInteractor(foo);
+    post.setReceiverInteractor(bar);
+
+    Mockito.when(interactorService.getInteractor(4L)).thenReturn(person);
+    Mockito
+      .when(interactionRepository.findActiveInteraction(120L))
+      .thenReturn(Optional.of(post));
+    Mockito.when(pageService.isPageManagedByPerson(21L, 4L)).thenReturn(true);
+
+    List<Interactor> possibleInteractors = interactionService.getAppropriateInteractorListForPerson(
+      4L,
+      120L
+    );
+    assertEquals(List.of(person, bar), possibleInteractors);
+    verify(interactorService, never())
+      .getRepresentableInteractorsByPerson(4L, 20L);
+  }
+
+  @Test
+  public void getAppropriateInteractorListForPersonDefaultsToRepresentableInteractorsInFrontOfFirstInteractor() {
+    Person person = new Person();
+    Page foo = new Page();
+    foo.setId(20L);
+    Page bar = new Page();
+    bar.setId(21L);
+
+    Post post = new Post();
+    post.setInteractor(foo);
+    post.setReceiverInteractor(bar);
+
+    Mockito.when(interactorService.getInteractor(4L)).thenReturn(person);
+    Mockito
+      .when(interactionRepository.findActiveInteraction(120L))
+      .thenReturn(Optional.of(post));
+    Mockito.when(pageService.isPageManagedByPerson(21L, 4L)).thenReturn(false);
+
+    interactionService.getAppropriateInteractorListForPerson(4L, 120L);
+    verify(interactorService, times(1))
+      .getRepresentableInteractorsByPerson(4L, 20L);
+  }
+
+  @Test
+  public void getAppropriateInteractorListForPersonOnlyAdmitsPersonAsFirstParameter() {
+    Page page = new Page();
+    Mockito.when(interactorService.getInteractor(4L)).thenReturn(page);
+
+    assertThrows(
+      ClassCastException.class,
+      () -> {
+        interactionService.getAppropriateInteractorListForPerson(4L, 120L);
       }
     );
   }
